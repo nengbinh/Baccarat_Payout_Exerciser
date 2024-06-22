16fa543e64d5c8c92c78b48c0a52e47102b1b84f
nengbinh <82856938+nengbinh@users.noreply.github.com>
2024-06-22 14:19:27 +0800

    3

--- js/button.js
+++ js/button.js
@@ -1,10 +1,69 @@
 
+let result =
+    [
+        ['0', '', ''],
+        ['0', '', ''],
+        ['0', '', ''],
+        ['0', '', ''],
+        ['0', '', ''],
+    ]
+let result_index = 0
+
+
+//
+$(document).ready(function () {
+    show_result()
+})
+
+// 显示结果
+function show_result() {
+    // 获取对象
+    let result_obj = $('.result .round')
+    // 循环
+    result.forEach(function(data, index){
+        if (data[1] !== '') {
+            result_obj.eq(index).html(`
+                <span>#${index + 1}</span>
+                <span>${formatTime(Number(data[0]))}</span>
+                <span style="color: ${ data[2] === 'c' ? '#33c53e' : 'red' }">${data[1]}</span>
+            `)
+        }
+    })
+}
+
+// 数字转换成文本
+function formatTime(minutes) {
+    // 计算小时和分钟
+    let hours = Math.floor(minutes / 60);
+    let mins = minutes % 60;
+
+    // 格式化输出
+    let formattedHours = hours < 10 ? `0${hours}` : `${hours}`;
+    let formattedMins = mins < 10 ? `0${mins}` : `${mins}`;
+
+    return `${formattedHours}:${formattedMins}`;
+}
+
+// 启动计时器
+setInterval(function () {
+    let timer = $("#ch_timer")
+    let clock = $("#ch_clock span")
+    let timer_num = Number(timer.html())
+    if (timer_num !== 0) {
+        timer_num += 1
+        timer.html(timer_num)
+        clock.html(formatTime(timer_num))
+    }
+}, 1000);
+
+// 按钮点击
 function start_action() {
     let button = $('#mainButton')
     let button_text = button.text()
+    let timer = $("#ch_timer")
 
     // 开始游戏
-    if (button_text === 'Start') {
+    if (button_text === 'Start' || button_text === 'Next') {
         let chip = create_chip_data()
         // 显示筹码
         show_chip(chip.chip_data)
@@ -12,6 +71,8 @@ function start_action() {
         $('#ch_answer').html(chip.total_value)
         // 更新按钮
         button.text('Answer')
+        // 更新时间
+        timer.html('1')
     }
 
     // 进行回答
@@ -28,13 +89,46 @@ function start_action() {
         let user_answer = prompt("What is the answer?", "")
         if (user_answer != null) {
 
+            // 更新结果
             if (Number(user_answer) !== real_answer) {
                 alert('(回答错误) - 正确答案是 ' + real_answer)
+                result[result_index] = [timer.html(), `${user_answer}_${real_answer}`, 'w']
             }else{
                 alert('Very Good (回答正确)')
+                result[result_index] = [timer.html(), `${real_answer}_${real_answer}`, 'c']
             }
-            
+            result_index += 1
+            show_result()
+            // 判断游戏是否玩完了
+            if (result_index === 5) {
+                // 更新按钮
+                button.text('Exit')
+            }else{
+                //
+                alert('Click Button to Continue (点击按钮继续下个游戏)')
+                // 更新按钮
+                button.text('Next')
+            }
+            // 更新时间
+            timer.html('0')
         }
     }
 
+    // 退出游戏
+    if (button_text === 'Exit'){
+        location.reload()
+    }
+
+}
+
+// 选择玩法
+function select_game(game_type) {
+    $('.selector').css('display', 'none')
+    $('#ch_type').html(game_type)
+    let img = $("#ch_clock img")
+    if (game_type === 'panda') {
+        img.attr('src', 'img/panda.jpg')
+    }else{
+        img.attr('src', 'img/dragon.png')
+    }
 }
